c9eb0f8ac3ff1f7e4d40d4a61662653c5b338351
Ted Barnett <mail@tedbarnett.com>
2020-01-05 17:36:10 -0800

    Behavior sliders working!

--- Assets/Scripts/FlockingApp.cs
+++ Assets/Scripts/FlockingApp.cs
@@ -12,22 +12,28 @@ public class FlockingApp : MonoBehaviour
     private DeviceOrientation lastOrientation;
     private bool isPortraitMode;
     //private Vector2 fingerDelta;
-    public Slider separationWeightSlider; // used to set FlockBox Separation Behavior
+    public Slider containmentWeightSlider; // used to set FlockBox Behaviors
+    public Slider alignmentWeightSlider;
+    public Slider cohesionWeightSlider;
+    public Slider separationWeightSlider;
+    //public enum behaviorType { Containment, Alignment, Cohesion, Separation };
 
     private CloudFine.FlockBox flockBox;
+    public CloudFine.BehaviorSettings boidSettings;
+    public List<Slider> AllSliders = new List<Slider>();
 
-    //TODO: How do I access (and change) FlockBox Behavior values?
-    //public float containmentBehaviorWeight;
-    //public CloudFine.AlignmentBehavior alignmentBehavior;
-    //public CloudFine.CohesionBehavior cohesionBehavior;
-    public CloudFine.SeparationBehavior separationBehavior; // does not seem to work!
 
     void Start()
     {
+        AllSliders.Add(containmentWeightSlider);
+        AllSliders.Add(alignmentWeightSlider);
+        AllSliders.Add(cohesionWeightSlider);
+        AllSliders.Add(separationWeightSlider);
+
         flockBox = GetComponent<CloudFine.FlockBox>();
         cameraTransform = FindObjectOfType<Camera>().GetComponent<Transform>();
         //separationWeightSlider = GetComponent<Slider>();
-        separationWeightSlider.GetComponent<Slider>().onValueChanged.AddListener(delegate { SliderChanged(); });
+        //separationWeightSlider.GetComponent<Slider>().onValueChanged.AddListener(delegate { SliderChanged(behaviorType behaviorIs); });
 
         lastOrientation = Input.deviceOrientation;
         statusText.text = "";
@@ -41,11 +47,99 @@ public class FlockingApp : MonoBehaviour
         ChangeOrientation();
     }
 
-    public void SliderChanged() // TODO: Make FlockBox Behaviors change here!
+    public void SliderChanged(string sliderID) // TODO: Make FlockBox Behaviors change here!
     {
-        statusText.text = "Separation: " + separationWeightSlider.value.ToString();
-        separationBehavior.weight = separationWeightSlider.value; //ERROR: not set to an instance of an object?
 
+        //separationBehavior.weight = separationWeightSlider.value; //ERROR: not set to an instance of an object?
+
+        // Iterate through the array of all boids 
+
+        foreach(CloudFine.SteeringBehavior behavior in boidSettings.Behaviors)
+        {
+
+            if ((sliderID == "containment") && (behavior.GetType()==typeof(CloudFine.ContainmentBehavior)))
+                {
+                    behavior.weight = containmentWeightSlider.value;
+                    statusText.text = sliderID + ": " + containmentWeightSlider.value.ToString();
+                    FadeOthers(containmentWeightSlider);
+
+            }
+            if ((sliderID == "alignment") && (behavior.GetType() == typeof(CloudFine.AlignmentBehavior)))
+            {
+                behavior.weight = alignmentWeightSlider.value;
+                statusText.text = sliderID + ": " + alignmentWeightSlider.value.ToString();
+                FadeOthers(alignmentWeightSlider);
+
+            }
+            if ((sliderID == "cohesion") && (behavior.GetType() == typeof(CloudFine.CohesionBehavior)))
+            {
+                behavior.weight = cohesionWeightSlider.value;
+                statusText.text = sliderID + ": " + cohesionWeightSlider.value.ToString();
+                FadeOthers(cohesionWeightSlider);
+
+            }
+            if ((sliderID == "separation") && (behavior.GetType() == typeof(CloudFine.SeparationBehavior)))
+            {
+                behavior.weight = separationWeightSlider.value;
+                statusText.text = sliderID + ": " + separationWeightSlider.value.ToString();
+                FadeOthers(separationWeightSlider);
+
+            }
+
+
+        }
+
+    }
+
+    public void SliderTouched(string sliderID)
+    {
+        if (sliderID == "containment")
+        {
+            FadeOthers(containmentWeightSlider);
+        }
+        if (sliderID == "alignment")
+        {
+            FadeOthers(alignmentWeightSlider);
+        }
+        if (sliderID == "cohesion")
+        {
+            FadeOthers(cohesionWeightSlider);
+        }
+        if (sliderID == "separation")
+        {
+            FadeOthers(separationWeightSlider);
+        }
+    }
+
+    private void FadeOthers(Slider sliderTouched)
+    {
+        Color fadedColor = new Color(1.0f, 1.0f, 1.0f, 0.1f);
+        Color visibleColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        Color inVisibleColor = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+        foreach (Slider thisSlider in AllSliders)
+        {
+            var allImages = thisSlider.GetComponentsInChildren<Image>();
+            foreach (Image image in allImages)
+            {
+                image.color = fadedColor;
+            }
+            var allText = thisSlider.GetComponentInChildren<Text>();
+            allText.color = fadedColor;
+            Image buttonImage = thisSlider.GetComponentInChildren<Button>().GetComponentInChildren<Image>();
+            buttonImage.color = inVisibleColor;
+        }
+
+        // De-fade the one chosen
+        var thisImages = sliderTouched.GetComponentsInChildren<Image>();
+        foreach (Image image in thisImages)
+        {
+            image.color = visibleColor;
+        }
+        var thisText = sliderTouched.GetComponentInChildren<Text>();
+        thisText.color = visibleColor;
+        Image thisButtonImage = sliderTouched.GetComponentInChildren<Button>().GetComponentInChildren<Image>();
+        thisButtonImage.color = inVisibleColor;
     }
 
     // TODO: Replace sliders with LeanTouch finger gestures (swipes to change Flock Behavior values)
@@ -99,12 +193,4 @@ public class FlockingApp : MonoBehaviour
         lastOrientation = Input.deviceOrientation;
     }
 
-    void OnApplicationPause(bool paused)
-    {
-        if (!paused)
-        {
-            // Application came back to the fore; double-check authentication
-            statusText.text = "Just opened app at " + System.DateTime.Now; //TODO: Testing this for Photo Reviewer app
-        }
-    }
 } // class
