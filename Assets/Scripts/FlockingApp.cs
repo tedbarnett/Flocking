5fe7e6b2de0cb30ec4f22df5f32b74e8d597d8b9
Ted Barnett <mail@tedbarnett.com>
2020-01-06 12:39:09 -0800

    Enabled a trackable bird (larger and red), cleaned up files

--- Assets/Scripts/FlockingApp.cs
+++ Assets/Scripts/FlockingApp.cs
@@ -1,16 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-//using Lean.Touch;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class FlockingApp : MonoBehaviour
 {
-    private Transform cameraTransform;
-    private Vector3 cameraPosition;
-    public Text statusText;
-    private DeviceOrientation lastOrientation;
-    private bool isPortraitMode;
     public Slider birdCountSlider;
     public Slider containmentWeightSlider; // used to set FlockBox Behaviors
     public Slider alignmentWeightSlider;
@@ -18,13 +10,21 @@ public class FlockingApp : MonoBehaviour
     public Slider separationWeightSlider;
     public GameObject companyLogo;
     public GameObject behaviorPanels;
-    private bool firstFrame = true;
-    public float flockBoxLength = 21.0f;
-    public float cameraBaseZ = 200.0f;
-
+    public float flockBoxLength = 21.0f; // long edge of flockBox
+    public Text statusText; // used for on-screen debug messaging
     private CloudFine.FlockBox flockBox;
     public CloudFine.BehaviorSettings boidSettings;
     public int maxBirds = 500;
+
+    private bool firstFrame = true;
+    private bool isPortraitMode;
+    private bool wasPortraitMode;
+    private Transform cameraTransform;
+    private Vector3 cameraPosition;
+    private float newWidth;
+    private float newHeight;
+    private float cameraZ;
+    private int lastHeight;
 
     void Start()
     {
@@ -33,17 +33,11 @@ public class FlockingApp : MonoBehaviour
         flockBox = GetComponent<CloudFine.FlockBox>();
         cameraTransform = FindObjectOfType<Camera>().GetComponent<Transform>();
 
-        lastOrientation = Input.deviceOrientation;
-        if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight)
-        {
-            isPortraitMode = false;
-        }
-        else
-        {
-            isPortraitMode = true;
-        }
-        isPortraitMode = !isPortraitMode;
-        ChangeOrientation(); // this will set up the FlockBox size, then flip the isPortraitMode back to the correct value
+        isPortraitMode = (Screen.width < Screen.height);
+        wasPortraitMode = isPortraitMode; // used to watch for screen orientation changes
+        lastHeight = Screen.height;
+
+        ChangeOrientation(); // this will set up the correct FlockBox size and camera angle
         statusText.text = "";
 
     }
@@ -53,13 +47,15 @@ public class FlockingApp : MonoBehaviour
         if (firstFrame)
         {
             UpdateBirds(Mathf.RoundToInt(birdCountSlider.value)); // set bird count to current position of birdCountSlider
-            statusText.text = "screenX: " + Screen.width + ", screenY: " + Screen.height;
-            statusText.text = statusText.text + "\n" + "boxX: " + flockBox.dimensions_x + ", screenY: " + flockBox.dimensions_y;
-
+            // set up Tracked Bird (2x size and Red in color)
+                Transform trackedBird = this.gameObject.transform.GetChild(0).GetChild(0);
+                trackedBird.localScale = new Vector3(2.0f * trackedBird.localScale.x, 2.0f * trackedBird.localScale.y, 2.0f * trackedBird.localScale.z);
+                Material trackedBirdMaterial = this.gameObject.transform.GetChild(0).GetChild(0).GetComponent<Renderer>().material;
+                trackedBirdMaterial.color = Color.red;
             firstFrame = false;
         }
-        if (lastOrientation == Input.deviceOrientation) return;
-        ChangeOrientation();
+        isPortraitMode = (Screen.width < Screen.height);
+        if ((lastHeight != Screen.height) || (isPortraitMode != wasPortraitMode)) ChangeOrientation();
     }
 
     public void SliderChanged(string sliderID)
@@ -105,55 +101,56 @@ public class FlockingApp : MonoBehaviour
 
     private void ChangeOrientation()
     {
-        isPortraitMode = !isPortraitMode;
-        float cameraZ = 0;
+        isPortraitMode = (Screen.width < Screen.height);
+
+        float cameraFOV = Camera.main.fieldOfView;
+        float cameraFOVRadians = cameraFOV * Mathf.Deg2Rad;
+        float triangleTangent = Mathf.Tan(cameraFOVRadians/2.0f);  // e.g. 60 degree camera angle implies z = (height/2)/(tan(30))
+        float cellSize = (float)flockBox.cellSize;
+        float fudge = 1.10f; // tweak the camera position out 10% further than computed
 
         RectTransform birdCountRectTransform = birdCountSlider.GetComponent<RectTransform>();
 
         // TODO: Replace the hard-wired dimensions (and cam position) below with a formula based on current screen resolution
 
-        if (!isPortraitMode || Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+        if (!isPortraitMode)
         {
             // Device is in landscape mode
-            //cameraPosition = new Vector3(94.0f, 58.0f, -102f);
-            //cameraTransform.position = cameraPosition;
-            flockBox.dimensions_x = Mathf.RoundToInt(flockBoxLength);
-            flockBox.dimensions_y = Mathf.RoundToInt(flockBoxLength * ((float)Screen.height/(float)Screen.width));
-            cameraZ = -cameraBaseZ * ((float)Screen.height / (float)Screen.width);
-
-            //companyLogo.transform.gameObject.SetActive(false); // hide logo in landscape mode (too crowded)
+            newWidth = flockBoxLength; // width is the "long edge" in portrait mode
+            newHeight = newWidth * ((float)Screen.height/(float)Screen.width);
         }
         else
         {
             // Device is in portrait mode
-            //cameraPosition = new Vector3(55.0f, 112.0f, -202.0f);
-            //cameraTransform.position = cameraPosition;
-            flockBox.dimensions_x = Mathf.RoundToInt(flockBoxLength * ((float)Screen.width / (float)Screen.height));
-            flockBox.dimensions_y = Mathf.RoundToInt(flockBoxLength);
-            cameraZ = -cameraBaseZ;
-
-            //companyLogo.transform.gameObject.SetActive(true); // hide logo in landscape mode (too crowded)
-
+            newHeight = flockBoxLength; // height is the "long edge" in portrait mode
+            newWidth = newHeight * ((float)Screen.width / (float)Screen.height);
         }
+        flockBox.dimensions_x = Mathf.RoundToInt(newWidth);
+        flockBox.dimensions_y = Mathf.RoundToInt(newHeight);
+        cameraZ = -(fudge) * ((newHeight * cellSize) / 2.0f) / triangleTangent; // 60 degree camera angle implies z = (height/2)/(tan(30))
+
         statusText.text = "screenX: " + Screen.width + ", screenY: " + Screen.height;
         statusText.text = statusText.text + "\n" + "boxX: " + flockBox.dimensions_x + ", screenY: " + flockBox.dimensions_y;
+        statusText.text = statusText.text + "\n" + "cameraZ: " + cameraZ;
 
-        cameraPosition = new Vector3((flockBox.dimensions_x * flockBox.cellSize)/2, (flockBox.dimensions_y * flockBox.cellSize) / 2, cameraZ);
+        cameraPosition = new Vector3(5.0f + (newWidth * cellSize) /2, (newHeight * cellSize) / 2, cameraZ);
         cameraTransform.position = cameraPosition;
 
-
-        lastOrientation = Input.deviceOrientation;
+        companyLogo.transform.gameObject.SetActive(isPortraitMode); // hide logo in landscape mode (too crowded)
+        wasPortraitMode = isPortraitMode; // reset comparison bool
+        lastHeight = Screen.height;
     }
 
     public void UpdateBirds(int birdCount)
     {
         int i = 0;
         Transform flockParent = this.gameObject.transform;
+
         foreach (Transform child in flockParent)
         {
             if (child != flockParent)
             {
-                if(i < birdCount)
+                if (i < birdCount)
                 {
                     child.transform.gameObject.SetActive(true); // show all birds up to "birdCount"
                 } else
